54779ef40a7b784054208e8267b1c119bcb2e023
Brian Serein <bserein22@gmail.com>
2022-03-01 14:56:10 -0500

    added hashed passwords and an authorization token

--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -1,17 +1,22 @@
 import { useState } from "react"
+import bcrypt from "bcryptjs";
+
+// npm i bcryptjs //use this to install the bcrypt to use salt
+const salt = '$2b$10$Ak0P/Qh2F6Z8WGg8DAHgoe'
 
 export default function Login({setToken, setIsUser}){
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    const hashedPassword = bcrypt.hashSync(password, salt) //created the bcrypt hashpassword, in place of password now
     fetch('http://localhost:3001/users/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify({ email, password: hashedPassword })
     })
     .then(response => response.json())
     .then(data => setToken(data.token))
